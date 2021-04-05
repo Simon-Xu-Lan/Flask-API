a72533b0462f00760264db9ea86b4e365fd44c6b
Simon Xu <xulan20907@gmail.com>
2021-04-04 22:09:30 -0600

    Add data.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, render_template, request
 import json
+from data import samples
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -10,9 +12,9 @@ def home():
     
 @app.route("/random", methods=["GET"])
 def get_random_cafe():
-    with open('samples.json') as f:
-        data = json.load(f)
-    return jsonify(data)
+    # with open('samples.json') as f:
+    #     data = json.load(f)
+    return jsonify(samples)
 
 
 if __name__ == '__main__':
